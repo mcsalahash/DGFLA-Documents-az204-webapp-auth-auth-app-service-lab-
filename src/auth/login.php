721081e4e4efc928253766640e5fec78914185be
Salah Eddine ELMACHKOURY <Salah@atec.org.ma>
2025-05-04 21:21:32 +0200

    Correction du fichier login.php et mise à jour de la configuration4

--- src/auth/login.php
+++ src/auth/login.php
@@ -4,47 +4,28 @@ session_start();
 
 $provider = $_GET['provider'] ?? '';
 
-if (!isset($authProviders[$provider])) {
-    handleError('Fournisseur d\'authentification non valide', 400);
-}
-
-$providerConfig = $authProviders[$provider];
-
-switch ($provider) {
-    case 'microsoft':
-        // Génération d'un état pour éviter les attaques CSRF
-        $_SESSION['state'] = bin2hex(random_bytes(16));
-        
-        // Construction de l'URL d'autorisation
-        $authUrl = 'https://login.microsoftonline.com' . $providerConfig['tenant'] . '/oauth2/v2.0/authorize';
-        $authUrl .= '?client_id=' . urlencode($providerConfig['client_id']);
-        $authUrl .= '&response_type=code';
-        $authUrl .= '&redirect_uri=' . urlencode($providerConfig['redirect_uri']);
-        $authUrl .= '&scope=' . urlencode($providerConfig['scope']);
-        $authUrl .= '&state=' . urlencode($_SESSION['state']);
-        
-        // Redirection vers la page d'authentification Microsoft
-        header('Location: ' . $authUrl);
-        exit;
-        
-    case 'google':
-        // Code similaire pour Google OAuth2
-        $_SESSION['state'] = bin2hex(random_bytes(16));
-        
-        $authUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-        $authUrl .= '?client_id=' . urlencode($providerConfig['client_id']);
-        $authUrl .= '&response_type=code';
-        $authUrl .= '&redirect_uri=' . urlencode($providerConfig['redirect_uri']);
-        $authUrl .= '&scope=' . urlencode($providerConfig['scope']);
-        $authUrl .= '&state=' . urlencode($_SESSION['state']);
-        
-        header('Location: ' . $authUrl);
-        exit;
-        
-    case 'custom':
-        // Exemple d'authentification personnalisée
-        // Implémentation d'un système basé sur un nom d'utilisateur/mot de passe
-        include '../views/custom-login.php';
-        exit;
+if ($provider === 'microsoft') {
+    // Génération d'un état pour éviter les attaques CSRF
+    $_SESSION['state'] = bin2hex(random_bytes(16));
+    
+    // Valeurs codées en dur pour le test
+    $tenant = '808efd5-5728-459b-800c-968535a011a1';
+    $clientId = '3cc14f09-1cd6-4717-981f-3482bc2fea35';
+    $redirectUri = 'https://authentiks.azurewebsites.net/src/auth/callback.php?provider=microsoft';
+    $scope = 'openid profile email';
+    
+    // Construction de l'URL d'autorisation
+    $authUrl = 'https://login.microsoftonline.com/' . $tenant . '/oauth2/v2.0/authorize';
+    $authUrl .= '?client_id=' . urlencode($clientId);
+    $authUrl .= '&response_type=code';
+    $authUrl .= '&redirect_uri=' . urlencode($redirectUri);
+    $authUrl .= '&scope=' . urlencode($scope);
+    $authUrl .= '&state=' . urlencode($_SESSION['state']);
+    
+    // Redirection vers la page d'authentification Microsoft
+    header('Location: ' . $authUrl);
+    exit;
+} else {
+    echo "Fournisseur d'authentification non valide ou non spécifié";
 }
 ?>
